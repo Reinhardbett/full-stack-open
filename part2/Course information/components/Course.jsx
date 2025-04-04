a222788660e8adefbc66565a22ad55b00b79aa2b
Reinhardbett <bettkiptoo46@gmail.com>
2025-04-04 10:11:22 +0300

    Rendering a ollection & separating logic into modules

---
+++ part2/Course information/components/Course.jsx
@@ -0,0 +1,34 @@
+const Header = (props) => <h1>{props.course}</h1>
+
+const Content = ({ parts }) => (
+<div>
+    <ul>
+    {parts.map((part) => 
+        <li key={part.id}>
+        {part.name} {part.exercises}
+        </li>
+    )}
+    </ul>
+</div>
+)
+
+const Total = ({ parts }) => {
+const totalExercises = parts.reduce((sum, parts) => sum + parts.exercises, 0)
+    return (
+    <div>
+        <p>total of { totalExercises } exercises</p>
+    </div>
+    );
+};
+
+const Course = ({ course }) => {
+    return (
+        <div>
+        <Header course={course.name} />
+        <Content parts={course.parts} />
+        <Total parts={course.parts} />
+        </div>
+    );
+    };
+
+export default Course;
