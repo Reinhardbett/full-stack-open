d04a41af87e7530d430d6adf379f8003a1bae8b4
reinhardtbett <bettkiptoo46@gmail.com>
2025-09-08 17:09:22 +0300

    Project initialization

---
+++ part5/bloglist-frontend/README.md
@@ -0,0 +1,8 @@
+# Exercise 5.1.
+- [ ] Create login form (username and password)
+- [ ] If a user is not logged in, only the login form should be visible
+    - [ ] Save the token returned from a successful login to the application's state user
+- [ ] If the user is logged-in name of the user and a list of blogs should be shown
+# Exercise 5.2.
+# Exercise 5.3.
+# Exercise 5.4.
---
+++ part5/bloglist-frontend/src/App.jsx
@@ -0,0 +1,47 @@
+import { useState, useEffect } from 'react'
+import Blog from './components/Blog'
+import blogService from './services/blogs'
+
+const App = () => {
+  const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  useEffect(() => {
+    blogService.getAll().then(blogs =>
+      setBlogs( blogs )
+    )  
+  }, [])
+
+  const handleLogin = (event) => {
+    event.preventDefault()
+    blogService.login({ username, password })
+      .then(user => {
+        setUser(user)
+        setUsername('')
+        setPassword('')
+      })
+  }
+
+  if (user === null) {
+    return (
+      <div>
+        <h2>Log in to application</h2>
+        <form>
+          //...
+        </form>
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      <h2>blogs</h2>
+      {blogs.map(blog =>
+        <Blog key={blog.id} blog={blog} />
+      )}
+    </div>
+  )
+}
+
+export default App
